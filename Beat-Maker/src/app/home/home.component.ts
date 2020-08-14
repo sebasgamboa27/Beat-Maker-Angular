a3cffe73edf2cae8245a8de1528f390c04654c77
SebasG <gamboasebas@gmail.com>
2020-08-13 19:23:10 -0600

    Piano Implemented

--- Beat-Maker/src/app/home/home.component.ts
+++ Beat-Maker/src/app/home/home.component.ts
@@ -1,95 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as Tone from 'node_modules/tone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
 
-  kick = new Tone.Player("./drums/kick-electro01.wav").toMaster();
-  snare = new Tone.Player("./drums/snare-lofi02.wav").toMaster();
-  clap = new Tone.Player("./drums/clap-analog.wav").toMaster();
-  crash = new Tone.Player("./drums/crash-808.wav").toMaster();
-  tom = new Tone.Player("./drums/tom-808.wav").toMaster();
-  openhat = new Tone.Player("./drums/openhat-808.wav").toMaster();
-  cowbell = new tone.Player("./drums/cowbell-808.wav").toMaster();
-	hihat = new Tone.Player("./drums/hihat-digital.wav").toMaster();
-  index = 0;
+  name = 'Angular Tone.js';
+  synth:any;
+  notes:string[] = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
+	octives:number[] = [1,2,3,4,5,6];
+	
+	activePhaser: boolean;
+	activeChorus: boolean;
 
+	chorusTone = new Tone.Chorus(4, 2.5, 0.5);
+	phaserTone = new Tone.Phaser({
+		"frequency" : 2, 
+		"octaves" : 2, 
+		"baseFrequency" : 400
+	}).toMaster();
 
-  constructor() { }
+ 
+	msdown:boolean = false;
+	
+	@Input() kick1: boolean;
 
-  ngOnInit(): void {
-		const synth = new Tone.Player();
-		
-    synth.triggerAttackRelease("C4", "8n");
+  constructor() {
+    this.synth = new Tone.PolySynth( Tone.Synth).toMaster();
+	}
+	
+	ngOnInit(): void {
+		this.activeChorus = false;
+		this.activePhaser = false;
+	}
+
+  chorus() {
+		if(this.activeChorus){
+			this.synth.disconnect(this.chorusTone);
+			this.activeChorus = false;
+		}
+		else{
+			this.synth = new Tone.PolySynth(Tone.MonoSynth).toMaster().connect(this.chorusTone);
+			this.activeChorus = true;
+		}
   }
 
-  sequencer() {
+  reverb(){
+    var reverb = new Tone.JCReverb(0.9).connect(Tone.Master);
+    var delay = new Tone.FeedbackDelay(0.2); 
+    this.synth = new Tone.DuoSynth().chain(delay, reverb);
+  }
+
+  phaser(){
+
+		if(this.activePhaser){
+			this.synth.disconnect(this.phaserTone);
+			this.activePhaser = false;
+		}
+		else{
+			this.synth.connect(this.phaserTone);
+			this.activePhaser = true;
+		}
     
-    Tone.Transport.scheduleRepeat(this.repeat, "8n");
-    Tone.Transport.start();
   }
 
-  repeat() {
-		let step = this.index % 8;
-		let kickInputs = document.querySelector(
-			`.kick .pad:nth-child(${step + 1}) input` //changed the format a bit to access the nth pad and then the input checkbox inside the nth pad
-		);
-		let snareInputs = document.querySelector(
-			`.snare .pad:nth-child(${step + 1}) input`
-		);
-		let clapInputs = document.querySelector(
-			`.clap .pad:nth-child(${step + 1}) input`
-		);
-		let crashInputs = document.querySelector(
-			`.crash .pad:nth-child(${step + 1}) input`
-		);
-		let tomInputs = document.querySelector(`
-        .tom .pad:nth-child(${step + 1}) input`);
-		let openhatInputs = document.querySelector(
-			`.openhat .pad:nth-child(${step + 1}) input`
-		);
-		let cowbellInputs = document.querySelector(
-			`.cowbell .pad:nth-child(${step + 1}) input`
-		);
-		let hihatInputs = document.querySelector(
-			`.hihat .pad:nth-child(${step + 1}) input`
-		);
-		if (kickInputs.checked) {
-			this.kick.start();
-		}
-		if (snareInputs.checked) {
-			this.snare.start();
-		}
-		if (clapInputs.checked) {
-			this.clap.start();
-		}
-		if (crashInputs.checked) {
-			this.crash.start();
-		}
-		if (tomInputs.checked) {
-			this.tom.start();
-		}
-		if (openhatInputs.checked) {
-			this.openhat.start();
-		}
-		if (cowbellInputs.checked) {
-			this.cowbell.start();
-		}
-		if (hihatInputs.checked) {
-			this.hihat.start();
-		}
-		this.index++;
-  }
-
-  changeColor(beat, index){
-    let pad = document.querySelector(`.${beat} .pad:nth-child(${index}) input`);
-    if (pad.checked === true) {
-    } else {
+  msover(note){
+    if(this.msdown){
+      this.play(note);
     }
-  
   }
+
+  play(note){
+     this.synth.triggerAttackRelease(note,"8n");
+	}
+
 }
+
